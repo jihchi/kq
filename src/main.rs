8a1f338792909314261737271e8567a60aee773a
jihchi <achi@987.tw>
2021-09-13 20:53:00 +0800

    Parse stdin as a document input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    todo!("TBD")
+use std::error;
+use std::io;
+
+fn main() -> Result<(), Box<dyn error::Error>> {
+    let mut lines = String::new();
+    io::stdin().read_line(&mut lines)?;
+    let _nodes = kdl::parse_document(lines)?;
+    Ok(())
 }
